991ec8cb30a30c045fd61a0824ecdb9b195086bc
Luke Perry <ldperry93@gmail.com>
2020-04-01 22:53:29 -0400

    spike on env parsing command line args

--- src/main.js
+++ src/main.js
@@ -1,3 +1,53 @@
 #!/usr/bin/env node
+const { argv } = require('yargs')
+    .option('dangerous', {
+        alias: 'd',
+        describe: 'will overwrite envs already in scope',
+        type: 'boolean',
+    })
+    .command({
+        command: 'config <key> [value]',
+        aliases: ['configure', 'cfg'],
+        desc: 'Set config value',
+        handler: (input) => {
+            console.log(`setting ${input.key} to ${input.value}`)
+        },
+    })
+    .command({
+        command: 'remote <action> <key> [value]',
+        aliases: ['r'],
+        desc: 'manage (add, remove) a remote ',
+        builder: (yargs) => {
+            yargs.command({
+                command: 'add <key> [value]',
+                desc: 'add a remote',
+                handler: (input) => {
+                    console.log(`add remote ${input.key} to ${input.value}`)
+                },
+            })
+        },
+    })
+    .command({
+        command: 'get',
+        desc: 'Retrieve envs',
+        builder: (yargs) => {
+            yargs.option('org', {
+                alias: 'organization',
+                describe: 'Org to retrieve values',
+                type: 'string',
+            })
+            yargs.option('app', {
+                alias: 'application',
+                describe: 'App to retrieve values',
+                type: 'string',
+            })
+        },
+        handler: (input) => {
+            console.log(`retrieve from org ${input.org} and app ${input.app}`)
+        },
+    })
+    .alias('version', 'v')
+    .alias('help', 'h')
+    .help()
 
-console.log('---------- hello world')
+console.log('---------- hello world', argv)
